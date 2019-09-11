0ff49dd94dbcafaf2efa0e837df64290d7ea78f0
Michał Bednarz <mibed90@gmail.com>
2019-09-11 12:27:54 +0200

    initial commit

---
+++ precache-manifest.c1a9c34debdc4b7109e8e9629deac369.js
@@ -0,0 +1,27 @@
+self.__precacheManifest = [
+  {
+    "url": "vendors~main.e64c242958fb16ea1731.bundle.js"
+  },
+  {
+    "revision": "570124de1674a2bdb1bd",
+    "url": "vendors~main.css"
+  },
+  {
+    "revision": "c2fed7c9a20d1e5d34bce328f7e83409",
+    "url": "static/media/right.c2fed7c9.svg"
+  },
+  {
+    "revision": "50f7c00f60268f9e97df8f9c369640e5",
+    "url": "static/media/left.50f7c00f.svg"
+  },
+  {
+    "url": "runtime~main.e64c242958fb16ea1731.bundle.js"
+  },
+  {
+    "url": "main.e64c242958fb16ea1731.bundle.js"
+  },
+  {
+    "revision": "a65ab488387c4bd3713ad9b06bd66513",
+    "url": "iframe.html"
+  }
+];
